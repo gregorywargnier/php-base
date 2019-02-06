1b31c6b4da75d33c7481af0dcdbd69c40c701765
Grégory wargnier <gregorywargnier@yahoo.fr>
2019-02-06 17:01:19 +0100

    my new exercice

---
+++ 04-boucles/exercice2.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * execrice étoile
+ */
+
+echo '<h2>exercice de l étoile</h2>';
+
+
+for ($i = 10; $i > 0; $i--){
+    for ($j = $i; $j > 0; $j--) {
+        echo '🔥';
+    }
+    echo '<br/>';
+}
+
+
+echo'--------------------------<br/>';
+
+for($i = 0; $i < 11; $i++){
+    $state = ($i === 5) ? 'visible': 'hidden';
+
+    echo '<span style="visibility: ' . $state. ' ">🔥</span>';
+}
+
