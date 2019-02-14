dbaeb2388b2c6ed5ab95e57dd5a0532a224c2ace
Grégory wargnier <gregorywargnier@yahoo.fr>
2019-02-14 12:38:15 +0100

    include

---
+++ 10-excercicesInclude/contact.php
@@ -0,0 +1,12 @@
+<?php
+include(__DIR__.'/partials/header.php');
+ ?>
+ <!-- Begin page content -->
+ <main role="main" class="container">
+      <h1 class="mt-5">contact</h1>
+      <p>ma page de contact</p>
+    </main>
+
+    <?php
+include(__DIR__.'/partials/footer.php');
+?>
---
+++ 10-excercicesInclude/partials/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+      <div class="container">
+        <span class="text-muted bg.light">Place sticky footer content here.</span>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+   
+
